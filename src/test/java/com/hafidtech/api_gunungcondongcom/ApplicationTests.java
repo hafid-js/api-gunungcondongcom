fa3bd926491f72d90d8a2cd77d1fac181a64995a
hafidjs <afidzpratama@gmail.com>
2024-07-21 15:12:42 +0700

    my first commit

---
+++ src/test/java/com/hafidtech/api_gunungcondongcom/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hafidtech.api_gunungcondongcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
